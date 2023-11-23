62c0741a201d4820ed615beab96ead50c5767027
Frerot <ntwalifrerotjacques@gmail.com>
2023-11-23 15:39:35 +0200

    innit commit ���

---
+++ script.js
@@ -0,0 +1,12 @@
+const humburger = document.querySelector(".humburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+const mobilenav = document.querySelector(".mobilenav");
+
+humburger.addEventListener("click", function () {
+  bar1.classList.toggle("animatebar1");
+  bar2.classList.toggle("animatebar2");
+  bar3.classList.toggle("animatebar3");
+  mobilenav.classList.toggle("opendrawer");
+});
